c2b306a733fd9633deba2ad0e6f3fb1e038a1c92
Alfredo Martínez <xshaffter@gmail.com>
2022-08-05 23:11:19 -0600

    [UPDATE] new lookups for DAL queriesgit add .

--- common/fastapi/db/utils.py
+++ common/fastapi/db/utils.py
@@ -7,6 +7,10 @@ from sqlalchemy.orm import declared_attr
 from common.fastapi.core.parameters import get_param_manager
 
 
+class QueryBuildingException(Exception):
+    pass
+
+
 class BaseModel(object):
     __abstract__ = True
 
@@ -24,11 +28,46 @@ vars = parameters.variables
 Model = TypeVar("Model", bound=BaseModel)
 
 
+def get_refs(key: str):
+    try:
+        column, escape = key.split('__')
+    except:
+        column = key
+        escape = None
+
+    return column, escape
+
+
+def define_operation(column, escaped_operation, value):
+    eq_result = column == value
+    results = {
+        'gt': column > value,
+        'gte': column >= value,
+        'lt': column < value,
+        'lte': column <= value,
+        'in': column in value,
+        'contains': value in column,
+        'iexact': value.lower() == column.lower(),
+        'ieq': value.lower() == column.lower(),
+        'exact': eq_result,
+        'eq': eq_result
+    }
+    if escaped_operation in results:
+        return results[escaped_operation]
+    elif escaped_operation is None:
+        return eq_result
+    else:
+        raise QueryBuildingException(
+            f"{escaped_operation} operation doesn't exist, please use one of the following: {results.keys()}")
+
+
 def query_perform(model: Model, **query):
     query_performed = []
     for key, value in query.items():
-        query_performed.append(getattr(model, key) == value)
-    print(query_performed)
+        column, escape = get_refs(key)
+        column_ref = getattr(model, column)
+        defined_operation = define_operation(column_ref, escape, value)
+        query_performed.append(defined_operation)
     return query_performed
 
 
